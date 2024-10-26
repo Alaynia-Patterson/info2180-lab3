e88ac8ac7e4f4a486069613410fcfb9e540f7433
Alaynia Patterson <alayniapatterson1100@gmail.com>
2024-10-26 03:19:34 -0500

    Exercise 4 - Check for the winner and update the status

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,6 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const squares = document.querySelectorAll('#board div');
     let currentPlayer = 'X'; // Begin with player 'X'
     const gameState = Array(9).fill(null); // Keep track of the board
+    const statusDiv = document.getElementById('status');
+
+
+    // Game winning patterns
+    const winningpatterns = [
+    [0, 1, 2], // Top row
+    [3, 4, 5], // Middle row
+    [6, 7, 8], // Bottom row
+    [0, 3, 6], // Left column
+    [1, 4, 7], // Middle column
+    [2, 5, 8], // Right column
+    [0, 4, 8], // Diagonal from top-left to bottom-right
+    [2, 4, 6]  // Diagonal from top-right to bottom-left
+    ];
 
     // Handle square click
     function handleSquareClick(event){
@@ -11,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const index = Array.from(squares).indexOf(squares);
 
         //Does nothing if the token is already taken
-        if (gameState[index] !== null){
+        if (gameState[index] !== null ||statusDiv.classList.contains('you-won')){
             return;
         }
 
@@ -20,9 +34,25 @@ document.addEventListener('DOMContentLoaded', () => {
         square.textContent = currentPlayer;
         square.classList.add(currentPlayer);
 
-        //Alternate the current player
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        //See if the current player won
+        if(checkWinner (currentPlayer)){
+            statusDiv.textContent = `Congratulations! ${currentPlayer} is the Winner!`;
+            statusDiv.classList.add('you-won');
+        } else {
+            //Alternate the current player
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
     }
+
+
+    // This function checks if the current player won
+    function checkWinner(player) {
+        return winningpatterns.some(patterns => {
+          return patterns.every(index => gameState[index] === player);
+        });
+    }
+
+        
 
     // Handle mouse over a square
     function handleMouseOver(event) {
