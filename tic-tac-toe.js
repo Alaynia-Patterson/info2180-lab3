75bd8ef9aa21ee217db7bdb5bfdaaf9cf797672a
Alaynia Patterson <alayniapatterson1100@gmail.com>
2024-10-26 02:14:42 -0500

    Exercise 2 - Add an X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,9 +2,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //Select all div elements in #board
     const squares = document.querySelectorAll('#board div');
+    let currentPlayer = 'X'; // Begin with player 'X'
+    const gameState = Array(9).fill(null); // Keep track of the board
 
-    //iterates through each div and adds the 'square' class
+    // Handle square click
+    function handleSquareClick(event){
+        const square = event.target;
+        const index = Array.from(squares).indexOf(squares);
+
+        //Does nothing if the token is already taken
+        if (gameState[index] !== null){
+            return;
+        }
+
+        //Update the game state and the UI
+        gameState[index] = currentPlayer;
+        square.textContent = currentPlayer;
+        square.classList.add(currentPlayer);
+
+        //Alternate the current player
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+    //Iterates through each div and adds the 'square' class
     squares.forEach(square => {
         squares.classList.add('square');
+        square.addEventListener('click', handleSquareClick);
     });
 });
